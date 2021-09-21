50a4bafcd63783490fcb043342444cf25428ff02
binay <1234Sabi>
2021-09-21 12:34:32 +0100

    Merge remote-tracking branch 'origin/Master' into Master
    # Conflicts:
    #	src/main/java/prgrammeForJenkins/JenkinsDemo.java

--- src/main/java/prgrammeForJenkins/JenkinsDemo.java
+++ src/main/java/prgrammeForJenkins/JenkinsDemo.java
@@ -3,5 +3,6 @@ package prgrammeForJenkins;
 public class JenkinsDemo {
     public static void main(String[] args) {
         System.out.println ("I am on on intellj");
+        System.out.println ("I am updated");
     }
 }
