5e6becbb34ac48b6d1314e4fb6de18bcca47d64b
binaya161 <90383987+binaya161@users.noreply.github.com>
2021-09-21 19:48:40 +0100

    Update JenkinsDemo.java
    new things have been added to the first sop.

--- src/main/java/prgrammeForJenkins/JenkinsDemo.java
+++ src/main/java/prgrammeForJenkins/JenkinsDemo.java
@@ -2,7 +2,7 @@ package prgrammeForJenkins;
 
 public class JenkinsDemo {
     public static void main(String[] args) {
-        System.out.println ("I am on on intellj");
+        System.out.println ("I am on on intellj  jenkins");
         System.out.println ("I am updated");
     }
 }
