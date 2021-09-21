4c122c81ac50ae8347d3d18d8c89b63757dbae47
binaya161 <90383987+binaya161@users.noreply.github.com>
2021-09-21 19:59:05 +0100

    This problem has been solved

--- src/main/java/prgrammeForJenkins/JenkinsDemo.java
+++ src/main/java/prgrammeForJenkins/JenkinsDemo.java
@@ -3,6 +3,6 @@ package prgrammeForJenkins;
 public class JenkinsDemo {
     public static void main(String[] args) {
         System.out.println ("I am on on intellj  jenkins Again");
-        System.out.println ("I am updated")
+        System.out.println ("I am updated");
     }
 }
